0de66ef741e68147de9f5b7a04ec9b00603c0013
Trygve <177544878+th926@users.noreply.github.com>
2026-05-07 10:14:06 +0200

    initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-I created this plugin in 46 houres and 34 minutes over the course of 8 days. So that is why the code looks like that :p
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <mysqlx/xdevapi.h>
+#include <iostream>
+
+int main(void) {
+    std::cout << "work\n";
+    return 0;
+}
+
+// 1. finne wp_config.php
+// 2. søke og slette
+// 3. fjerne seg selv
