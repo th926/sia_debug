717727ee3ad3abe17311ae0c32e89b7b531406e8
Trygve <177544878+th926@users.noreply.github.com>
2026-05-11 16:47:25 +0200

    added some work to the wp-config class

--- main.cpp
+++ main.cpp
@@ -32,6 +32,25 @@ std::string slurp(std::ifstream& in) {
     return sstr.str();
 }
 
+// DB_NAME
+// DB_USER
+// DB_PASSWORD
+// DB_HOST
+std::string regex_helper(std::string constant, std::string &content) {
+    std::string regex_str;
+    if (constant == "DB_HOST") { //(?:define\( *'DB_HOST', *')(\w+)(?::?)([0-9]*)(?:' *\)); this is the right one
+        regex_str = R"((?:define\( *')" + constant + R"(', *')(\w+)(?::)([0-9]+)(?:' *\));)";
+    } else {
+        regex_str = R"((?:define\( *')" + constant + R"(', *')(\w+)(?:' *\);))";
+    }
+    std::regex test = std::regex(regex_str);
+    std::smatch m;
+    if (std::regex_search(content, m, test)) {
+        return m[1];
+    }
+    return std::string();
+}
+
 class WPConfig {
 private:
     std::string db_name;
@@ -46,8 +65,17 @@ public:
     WPConfig() {
         std::ifstream in_configs (config_location, std::ios::in);
         std::string in_config = slurp(in_configs);
-        std::regex table_regex = std::basic_regex("(?:\$table_prefix = ')(.+)(?:')", std::regex::basic);
-
+        std::regex table_regex = std::basic_regex(R"((?:\$table_prefix *= *')(\w+)(?:' *;))", std::regex::basic);
+        db_name = regex_helper("DB_NAME", in_config);
+        db_user = regex_helper("DB_USER", in_config);
+        db_password = regex_helper("DB_PASSWORD", in_config);
+        db_host = regex_helper("DB_HOST", in_config);
+    }
+    void print() {
+        std::cout << db_name;
+        std::cout << db_password;
+        std::cout << db_user;
+        std::cout << db_host;
     }
 };
 
