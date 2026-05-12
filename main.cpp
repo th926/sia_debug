0263a42465d0529e791eba2f84c2c70e1a20e82b
Trygve <177544878+th926@users.noreply.github.com>
2026-05-12 09:46:07 +0200

    Configuration extraction and database connection - Class for wpconfig uses posix regex extended to extract the database information from wp-config.php - The information is used to create a database connection

--- main.cpp
+++ main.cpp
@@ -26,33 +26,26 @@ class wpdb {
     ~wpdb() {}
 };
 
+// check if file exists first
 std::string slurp(std::ifstream& in) {
     std::ostringstream sstr;
     sstr << in.rdbuf();
     return sstr.str();
 }
 
-// DB_NAME
-// DB_USER
-// DB_PASSWORD
-// DB_HOST
 std::string regex_helper(std::string constant, std::string &content) {
-    std::string regex_str;
-    if (constant == "DB_HOST") { //(?:define\( *'DB_HOST', *')(\w+)(?::?)([0-9]*)(?:' *\)); this is the right one
-        regex_str = R"((?:define\( *')" + constant + R"(', *')(\w+)(?::)([0-9]+)(?:' *\));)";
-    } else {
-        regex_str = R"((?:define\( *')" + constant + R"(', *')(\w+)(?:' *\);))";
-    }
-    std::regex test = std::regex(regex_str);
+    //R"((?:define\( *')" + constant + R"(', *')(\w+)(?::?)([0-9]*)(?:' *\));)"
+    std::string things = R"((define\( *')" + constant + R"(', *')([[:alnum:]_]+):?([[:digit:]]*)' *\);)";
+    std::regex test = std::regex(things, std::regex::extended);
     std::smatch m;
     if (std::regex_search(content, m, test)) {
-        return m[1];
+        return m[2];
     }
     return std::string();
 }
 
 class WPConfig {
-private:
+public:
     std::string db_name;
     std::string db_user;
     std::string db_password;
@@ -64,26 +57,30 @@ private:
 public:
     WPConfig() {
         std::ifstream in_configs (config_location, std::ios::in);
+        if (!in_configs.is_open()) {
+            std::cerr << "There is no wp-config.php in the current directory" << "\n";
+            exit(-1);
+        }
         std::string in_config = slurp(in_configs);
-        std::regex table_regex = std::basic_regex(R"((?:\$table_prefix *= *')(\w+)(?:' *;))", std::regex::basic);
-        db_name = regex_helper("DB_NAME", in_config);
-        db_user = regex_helper("DB_USER", in_config);
-        db_password = regex_helper("DB_PASSWORD", in_config);
-        db_host = regex_helper("DB_HOST", in_config);
-    }
-    void print() {
-        std::cout << db_name;
-        std::cout << db_password;
-        std::cout << db_user;
-        std::cout << db_host;
+        try {
+            std::regex table_regex = std::basic_regex(R"((?:\$table_prefix *= *')(\w+)(?:' *;))");
+
+            db_name = regex_helper("DB_NAME", in_config);
+            db_user = regex_helper("DB_USER", in_config);
+            db_password = regex_helper("DB_PASSWORD", in_config);
+            db_host = regex_helper("DB_HOST", in_config);
+        } catch (std::__1::regex_error &e) {
+            std::cerr << e.what() << "\n";
+            exit(-2);
+        }
     }
 };
 
 int main(void) {
-    // loot wp config.
+    WPConfig config{};
     try {
-        mysqlx::Session sess("127.0.0.1", 33060, "root", "123");
-        mysqlx::Schema db = sess.getSchema("absmnew"); // Needs to be figured out from wp_config.php
+        mysqlx::Session sess(config.db_host, 33060, config.db_user, config.db_password);
+        mysqlx::Schema db = sess.getSchema(config.db_name);
         mysqlx::Table table = db.getTable("wp_posts");
     } catch (const mysqlx::Error &err) {
         std::cerr << "MySQL Error: " << err.what() << std::endl;
